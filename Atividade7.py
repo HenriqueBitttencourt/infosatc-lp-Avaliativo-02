7be79c8d9e02f778471d5415b8443bca1b6ae5bd
HenriqueBitttencourt <zanyxplays@gmail.com>
2020-09-02 16:51:33 -0300

    feat: Atividade 07

---
+++ Atividade7.py
@@ -0,0 +1,5 @@
+componentes = ["AMD Ryzen Threadripper 3990X", "Geforce RTX 2080Ti", "2x Kingston HyperX fury 16GB", "Gabinete Cooler Master Cosmos C700M", "ASRock TRX40 Taichi", "Fonte corsair RM850", "SSD Adata XPG SX6000", "Water Cooler Nox Hummer H-120ARGB"]
+print(componentes)
+del componentes[1]
+del componentes[3]
+print(componentes)
